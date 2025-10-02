dba67d2d6dc3efa64974a7ff49b483aca276f8e1
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-10-02 13:27:43 -0400

    use version range for libjpeg turbo

--- conanfile.py
+++ conanfile.py
@@ -28,7 +28,7 @@ class ViamRealsense(ConanFile):
     def requirements(self):
         self.requires("viam-cpp-sdk/0.19.0")
         self.requires("librealsense/2.56.5")
-        self.requires("libjpeg-turbo/2.1.5")
+        self.requires("libjpeg-turbo/[>=2.1.0 <3]")
 
     def layout(self):
         cmake_layout(self, src_folder=".")
