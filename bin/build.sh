98a9ee2247960df3a6b2928299298b817f01eb27
Ethan Rodkin <ethan@viam.com>
2026-09-22 14:23:02 -0400

    build with debug info

--- bin/build.sh
+++ bin/build.sh
@@ -20,12 +20,14 @@ conan create . \
     -o "&:with_tests=False" \
     -pr:a "${PROFILE}" \
     -s:a compiler.cppstd=17 \
+    -s "viam-camera-realsense/*:build_type=RelWithDebInfo" \
     -c tools.system.package_manager:mode=install \
     --build=missing
 
 conan install --requires=viam-camera-realsense/0.0.1 \
     -pr:a "${PROFILE}" \
     -s:a compiler.cppstd=17 \
+    -s "viam-camera-realsense/*:build_type=RelWithDebInfo" \
     --lockfile-partial \
     --deployer-package "&" \
     --envs-generation false
